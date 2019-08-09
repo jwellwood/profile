d6e58847412bba5cc02a7a8b388b628568e560ea
jwellwood <joewellwood@gmail.com>
2019-08-09 14:53:23 +0200

    Updates

---
+++ precache-manifest.628a05ee287a9de25e03f5c3daa32b67.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b529d2e952a3e144f8e4a27519308607",
+    "url": "/profile/index.html"
+  },
+  {
+    "revision": "ac4c82fdb0b68167ed9f",
+    "url": "/profile/static/css/main.834fbd46.chunk.css"
+  },
+  {
+    "revision": "e146e473dcd176ecb9bf",
+    "url": "/profile/static/js/2.d0b30a27.chunk.js"
+  },
+  {
+    "revision": "ac4c82fdb0b68167ed9f",
+    "url": "/profile/static/js/main.ed36d841.chunk.js"
+  },
+  {
+    "revision": "189fd871db148202566e",
+    "url": "/profile/static/js/runtime~main.cc8686f1.js"
+  },
+  {
+    "revision": "0754bc9279a04aadbf6f76b56f6787d7",
+    "url": "/profile/static/media/faceimage.0754bc92.png"
+  },
+  {
+    "revision": "808f7b818bc1eaecd1e453ae643ce5ee",
+    "url": "/profile/static/media/footballimage.808f7b81.png"
+  },
+  {
+    "revision": "5d63305928b006be09426485211703db",
+    "url": "/profile/static/media/profile.5d633059.jpeg"
+  },
+  {
+    "revision": "05409d3ea688afd6201e9443ecf9cf1a",
+    "url": "/profile/static/media/reportimage.05409d3e.png"
+  }
+]);
